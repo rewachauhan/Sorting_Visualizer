05c38418a52ccefd43706a70cb1c6cf436989c53
rewachauhan <rewachauhan79@gmail.com>
2020-04-01 20:21:58 +0530

    quick sort and updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,12 +53,12 @@ export class AppComponent implements OnInit {
 sleep(s:number){
   return new Promise(resolve=>setTimeout(resolve,s)) //s is the count down value for timeout; timeout returns 1,2,3,4,5... to promise which is returned to await. as soon as the value of promise becomes 20 ,await contiues
 }
-async sort(n:number){
+async bubblesort(n:number){
   for (let i = 0; i < this.data.length; i++) {
 
     for (let j = 0; j < this.data.length-i-1; j++) {
-      this.chart.data.datasets[0].backgroundColor[j]= "#0080FF"
-      this.chart.data.datasets[0].backgroundColor[j+1]= "#0080FF"
+      this.chart.data.datasets[0].backgroundColor[j]= "#8FC1A9"
+      this.chart.data.datasets[0].backgroundColor[j+1]= "#8FC1A9"
       this.chart.update()
       if(this.data[j]<this.data[j+1] && n==1){
         var temp=this.data[j]
@@ -78,12 +78,60 @@ async sort(n:number){
         // console.log(this.chart.data)
         this.chart.update()
        }
-    await this.sleep(20)  //stays till this line until the fuction doesn't return anyth
+    await this.sleep(175)  //stays till this line until the fuction doesn't return anyth
     this.chart.data.datasets[0].backgroundColor[j]= "#FF9AA2"
     this.chart.data.datasets[0].backgroundColor[j+1]= "#FF9AA2"
     this.chart.update() 
     }
-    
   }
+}
+
+async quicksort(arr,lowindex,highindex){
+  if(lowindex<highindex){
+    var p = await this.partition(arr,lowindex,highindex)
+    await this.quicksort(arr,lowindex,p-1)
+    await this.quicksort(arr,p+1,highindex)
+  }
+}
+async partition(arr,lowindex,highindex){
+  var pivot=arr[highindex]
+  this.chart.data.datasets[0].backgroundColor[highindex]="#8FC1A9"
+  this.chart.update()
+  var i=lowindex-1
+  //this.chart.data.datasets[0].backgroundColor[i]="rgba(0,255,0,1)"
+  for (let j = lowindex; j < highindex; j++) {
+    this.chart.data.datasets[0].backgroundColor[j]="#9DBAD5"
+    this.chart.update()
+    if(arr[j]<pivot){
+      i++
+      var temp=arr[i]
+      arr[i]=arr[j]
+      arr[j]=temp
+      this.chart.data.datasets[0].data=arr
+      this.chart.data.labels=arr
+      this.chart.update()
+      await this.sleep(100)
+    }
+    this.chart.data.datasets[0].backgroundColor[j]="#FF9AA2"
+    //this.chart.data.datasets[0].backgroundColor[i]="#FF9AA2"
+    this.chart.update()
+    
+ }
+ this.chart.data.datasets[0].backgroundColor[i+1]="#9DBAD5"
+
+ var temp=arr[i+1]
+ arr[i+1]=arr[highindex]
+ arr[highindex]=temp
+ this.chart.data.datasets[0].backgroundColor[i+1]="#8FC1A9"
+ this.chart.data.datasets[0].backgroundColor[highindex]="#FF99A2"
+
+ this.chart.data.datasets[0].data=arr
+ this.chart.data.labels=arr
+ this.chart.update()
+ await this.sleep(20)
+ return i+1
+}
+qsort(){      //boiler function-> intermediate fn to call the main recursive fn; needed because the data being passed in the quicksort fn is being converted to local data and this this will edit the main data
+  this.quicksort(this.data,0,39) //since 
 }
 }
